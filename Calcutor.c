0ae33a5f173924da0c049dbe3441bba0a92e5233
Apiwich <nemesiscrisisart@gmail.com>
2026-02-01 13:34:21 +0700

    Plus

--- Calcutor.c
+++ Calcutor.c
@@ -9,4 +9,16 @@ int main() {
     HelloWorld();
 
     return 0;
+<<<<<<< HEAD
+=======
+}
+
+void HelloWorld() {
+    for (int i = 1; i <= 5; i++) {
+        printf("Hello World BASS\n");
+    }
+>>>>>>> 81750ef6758e30ef07fac762f228741d0d6e3cfe
+}
+void Calculate(){
+    scanf("%d");
 }
