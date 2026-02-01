ebb7160d370346375cb3f8d6e2b0bc89db9912ad
Sam_Mac <samkonmueng_sk@hotmail.com>
2026-02-01 15:14:19 +0700

    add Loop

--- Calcutor.c
+++ Calcutor.c
@@ -3,17 +3,17 @@
 #include <stdlib.h>
 void print_number(char **str_number);
 int main() {
-    int Number;
-    while (Number == 0)
-    { 
+    int Number = 0 ;
+   while (Number != 99)
+   {
         char *str_number;
-        printf("Input Number -->\n");
+        printf("Input Number -->");
         scanf("%d", &Number);
         snprintf(str_number, sizeof(str_number), "%d", Number);
         print_number(&str_number);
-    }
+   }    
     return 0;
 }
 void print_number(char **str_number){
-    printf("%s",*str_number);
+    printf("%s\n",*str_number);
 }
