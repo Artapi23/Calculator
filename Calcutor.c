b47499af9fb32438c074ee58c94c981c02dab0eb
siripong-ctn <siripong.2320@gmail.com>
2026-02-01 16:28:37 +0700

    Implement basic calculator functionality with switch-case for operations

--- Calcutor.c
+++ Calcutor.c
@@ -2,13 +2,37 @@
 
 int main() {
     int i, j, sum;
+    char s;
     printf("Enter your number 1: ");
     scanf("%d", &i);
+
+    printf("Select calculator (+ - * / %): ");
+    scanf("%s", &s);
 
     printf("Enter your number 2: ");
     scanf("%d", &j);
 
-    sum = i + j;
+    switch (s) {
+        case '+':
+            sum = i + j;
+            break;
+        case '-':
+            sum = i - j;
+            break;
+        case '*':
+            sum = i * j;
+            break;
+        case '/':
+            sum = i / j;
+            break;
+        case '%':
+            sum = i % j;
+            break;
+        default:
+            printf("Error calculator");
+            break;
+    }
+
     printf("Your answer is %d", sum);
     return 0;
 }
