af67d95f8263dd939a2c788323f6b2282cffb704
siripong-ctn <siripong.2320@gmail.com>
2026-02-01 13:45:21 +0700

    Try

--- Calcutor.c
+++ Calcutor.c
@@ -1,18 +1,9 @@
-// Git input-output test
 #include <stdio.h>
 
-void HelloWorld();
-
 int main() {
-    printf("Hello, World! By sam\n");
-
-    HelloWorld();
+    for (int i=1; i<=5; i++) {
+        printf("Hello World %d\n", i);
+    }
 
     return 0;
-}
-
-void HelloWorld() {
-    for (int i = 1; i <= 5; i++) {
-        printf("Hello World BASS\n");
-    }
 }
