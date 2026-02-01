d2a85766bb6d9ac997a9fa92af1d625a2f740e0d
siripong-ctn <siripong.2320@gmail.com>
2026-02-01 13:23:05 +0700

    Add HelloWorld function and update main to call it

--- Calcutor.c
+++ Calcutor.c
@@ -1,6 +1,18 @@
 // Git input-output test
 #include <stdio.h>
+
+void HelloWorld();
+
 int main() {
     printf("Hello, World! By sam\n");
+
+    HelloWorld();
+
     return 0;
+}
+
+void HelloWorld() {
+    for (int = 1; i <= 5; i++) {
+        printf("Hello World BASS\n");
+    }
 }
