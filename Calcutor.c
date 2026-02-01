feeff0d93073e9ec68b2bb11b14965715972c943
Apiwich <nemesiscrisisart@gmail.com>
2026-02-01 14:18:33 +0700

    :)

--- Calcutor.c
+++ Calcutor.c
@@ -1,24 +1,9 @@
-// Git input-output test
 #include <stdio.h>
-
-void HelloWorld();
-
-int main() {
-    printf("Hello, World! By sam\n");
-
-    HelloWorld();
-
-    return 0;
-<<<<<<< HEAD
-=======
+int main(){
+    int num ;
+    printf("Enter Number : ");
+    scanf("%d",&num);
 }
-
-void HelloWorld() {
-    for (int i = 1; i <= 5; i++) {
-        printf("Hello World BASS\n");
-    }
->>>>>>> 81750ef6758e30ef07fac762f228741d0d6e3cfe
-}
-void Calculate(){
-    scanf("%d");
+void Functionplus(){
+    
 }
